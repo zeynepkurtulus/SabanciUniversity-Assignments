e6eab76b42ce7d028ec06ae69858570e55f89398
Zeynep Kurtulus <96525796+zeynepkurtulus@users.noreply.github.com>
2023-06-05 14:33:44 +0300

    Add files via upload

---
+++ CS408/Term Project /TicTacToeClient/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace TicTacClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.buttonName = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(98, 34);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(415, 20);
+            this.txtIP.TabIndex = 0;
+            this.txtIP.Text = "192.168.2.7:9000";
+            this.txtIP.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(98, 95);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfo.Size = new System.Drawing.Size(415, 260);
+            this.txtInfo.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(98, 361);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(415, 20);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 364);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(353, 387);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(438, 387);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Location = new System.Drawing.Point(98, 60);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(330, 20);
+            this.txtName.TabIndex = 7;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // buttonName
+            // 
+            this.buttonName.Enabled = false;
+            this.buttonName.Location = new System.Drawing.Point(434, 58);
+            this.buttonName.Name = "buttonName";
+            this.buttonName.Size = new System.Drawing.Size(75, 23);
+            this.buttonName.TabIndex = 9;
+            this.buttonName.Text = "Send";
+            this.buttonName.UseVisualStyleBackColor = true;
+            this.buttonName.Click += new System.EventHandler(this.buttonName_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(434, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Disconnect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 387);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 435);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TCP/IP Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button buttonName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ CS408/Term Project /TicTacToeClient/Form1.cs
@@ -0,0 +1,237 @@
+﻿using SuperSimpleTcp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool disconnected = false;
+
+        SimpleTcpClient client;
+        bool inGame = false;
+        bool inQueue = false;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new SimpleTcpClient(txtIP.Text); //creates a new instance of a SimpleTcpClient object, passing in the text from a text box named txtIP as the IP address to connect to.
+                // initializes listeners for specific server events 
+                //client.Events.Connected += Events_Connected;
+                client.Events.DataReceived += Events_DataReceived;
+                client.Events.Disconnected += Events_Disconnected;
+                client.Connect();
+                btnConnect.Enabled = false;
+                txtName.Enabled = true;
+                buttonName.Enabled = true; //*
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error); // shows the message on the text box 
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            btnSend.Enabled = false;
+        }
+
+        //function that checks and states whether the server has disconnected 
+        private void Events_Disconnected(object sender, ConnectionEventArgs e)
+        {
+            if (!disconnected)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    txtInfo.Text += $"Server disconnected.{Environment.NewLine}";
+                });
+            }
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate // invokes a delegate on the UI thread so that the text box control can be updated. 
+            {
+                if (Encoding.UTF8.GetString(e.Data.ToArray()) == "Connected")
+                {
+                    disconnected = false;
+                    button1.Enabled=true;
+                    this.Invoke((MethodInvoker)delegate // invokes a delegate on the UI thread so that the text box control can be updated. 
+                    {
+                        txtInfo.Text += $"Server connected.{Environment.NewLine}"; // appends the text "Server connected" to the rich text box 
+                    });
+                }
+                else if (Encoding.UTF8.GetString(e.Data.ToArray()) == "four")
+                {
+                    disconnected = true;
+                    button1.Enabled = false;
+                    btnConnect.Enabled = true;
+                    txtName.Enabled = false;
+                    buttonName.Enabled = false;
+                    client.Disconnect();
+                    txtInfo.Text += $"Server Capacity(4) reached.{Environment.NewLine}"; // appends the text "Server connected" to the rich text box 
+                }
+                else
+                {
+
+                    txtInfo.Text += $"Server: {Encoding.UTF8.GetString(e.Data.ToArray())}{Environment.NewLine}"; // appends the text received from the server to the rich text box 
+                }
+                if (Encoding.UTF8.GetString(e.Data.ToArray()).Contains("You are X for this game.") || Encoding.UTF8.GetString(e.Data.ToArray()).Contains("You are O for this game."))
+                {
+                    inGame = true;
+                    inQueue = false;
+                }
+                // below lines check if the text received from the server matches any of the four conditions:
+                if (Encoding.UTF8.GetString(e.Data.ToArray()).Contains("It's your Turn"))
+                {
+                    //If the text contains the string "It's your Turn", the btnSend button is enabled so the user can make a move
+                    btnSend.Enabled = true;
+                }
+                else if (Encoding.UTF8.GetString(e.Data.ToArray()) == "Game end")
+                {
+                    // else if the text equals the string "Game end", the inGame flag is set to false meaning we are not in the game anymore and the btnSend button is enabled.
+                    
+                    if (!inQueue)
+                    {
+                        inGame = false;
+                        btnSend.Enabled = true;
+                    }
+                }
+                else if (Encoding.UTF8.GetString(e.Data.ToArray()) == "Invalid Username")
+                { //  else if If the text equals the string "Invalid Username", the buttonName and txtName controls are both enabled so the user can try again with a different username.
+                    buttonName.Enabled = true;
+                    txtName.Enabled = true;
+                }
+                else if (Encoding.UTF8.GetString(e.Data.ToArray()) == "Correct Username")
+                {
+                    //  If the text equals the string "Correct Username", the btnSend button is enabled meaning the user can now send a message
+                    btnSend.Enabled = true;
+                }
+                else if (Encoding.UTF8.GetString(e.Data.ToArray()) == "Waiting for another player to continue")
+                {
+                    btnSend.Enabled = true;
+                }
+            });
+        }
+
+        // a function to check and indicate that the Server has connected 
+        //private void Events_Connected(object sender, ConnectionEventArgs e)
+        //{
+        //    
+        //}
+
+        //this method handles the requests coming from the clients 
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected) // This line checks if the client is connected to the server.
+            { //below lines checks for the requests of the user like joinign or leaving the game 
+                if (txtMessage.Text == "join") //checks if the user's input in the txtMessage text box is equal to the string "join".
+                {
+                    inQueue = true;
+                    client.Send(txtMessage.Text); //send the message "join" to the server
+                    txtInfo.Text += $"Me: {txtMessage.Text}{Environment.NewLine}"; // append the text "Me: [message]" to the rich text box
+                    txtMessage.Text = string.Empty; // // clear the text box control txtMessage after the user has sent a message to the server this allows the user to type a new message without having to manually delete the old message
+                    if (!inGame) // check if the user is already in game, if not 
+                    {
+                        inGame = true; // make it true so that now the user is in the game
+                        btnSend.Enabled = false; //disable the send button 
+                    }
+                }
+                else if (txtMessage.Text == "leave")//checks if the user's input in the txtMessage text box is equal to the string "leave".
+                {
+                    
+                    client.Send(txtMessage.Text); // send the message "leave" to the server
+                    txtInfo.Text += $"Me: {txtMessage.Text}{Environment.NewLine}"; // append the text to the rich text box 
+                    
+                    txtMessage.Text = string.Empty; // clear the text box control txtMessage after the user has sent a message to the server this allows the user to type a new message without having to manually delete the old message
+                    if (inGame) // check if the user is in the game, if so 
+                    {
+                        inGame = false; // since the user is leaving the game set the inGame flag to false 
+                    }
+                }
+
+                else if (int.TryParse(txtMessage.Text, out int num) && num >= 1 && num <= 9) //checks if the user's input in the txtMessage text box can be parsed as an integer between 1 and 9, inclusive.
+                {
+                    client.Send(txtMessage.Text); // send the user's input to the server
+                    txtInfo.Text += $"Me: {txtMessage.Text}{Environment.NewLine}"; // append the text into rich text box 
+                    txtMessage.Text = string.Empty;  // clear the text box control txtMessage after the user has sent a message to the server this allows the user to type a new message without having to manually delete the old message
+                    if (inGame) // check if the user in the game, if so:
+                    {
+                        btnSend.Enabled = false; // disable the send button
+                    }
+                }
+                else if (!string.IsNullOrEmpty(txtMessage.Text)) // checks if the user's input in the txtMessage text box is not null or empty.
+                {
+                    client.Send(txtMessage.Text); // send the user's input to the server
+                    txtInfo.Text += $"Me: {txtMessage.Text}{Environment.NewLine}"; // append the text to the rich text box 
+                    txtMessage.Text = string.Empty; //clear the text box control txtMessage after the user has sent a message to the server this allows the user to type a new message without having to manually delete the old message
+                }
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonName_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected) // check if the client is connected to the server.
+            {
+                if (!string.IsNullOrEmpty(txtName.Text)) // if the user's input in the txtName text box is not null or empty.
+                {
+                    var mes = "0001" + txtName.Text; //create a message protocol; if the message starts with 0001 the server understands that the username of the client is actually provided 
+                    client.Send(mes); // sends the message stored in mes to the server using the client object.
+                    buttonName.Enabled = false;
+                    txtName.Enabled = false;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //disconnect button
+        {
+            disconnected = true;
+            button1.Enabled = false;
+            btnConnect.Enabled = true;
+            btnSend.Enabled = false;
+            txtName.Enabled = false;
+            buttonName.Enabled = false;
+            client.Disconnect();
+            txtInfo.Text += $"Server disconnected.{Environment.NewLine}"; // appends the text "Server connected" to the rich text box 
+        }
+
+        private void button2_Click(object sender, EventArgs e) // clear chat box
+        {
+            txtInfo.Text = "";
+        }
+    }
+}
